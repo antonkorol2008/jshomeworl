b92e759432bca035a367e74ff892dd44460a61a7
antonkorol2008 <antonkorol6r@gmail.com>
2024-09-16 18:26:04 +0300

    add.js

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let age = 16;
+
+console.log(age);
+let myName = "Anton"
+console.log(myName);
+let isStudent = true;
+console.log(isStudent);
+let myString = "Краще спробувати і пошкодувати, ніж шкодувати,що не пробував"
+console.log(myString);
+let myNumber = 66;
+myNumber = myNumber +10;
+console.log(myNumber);
+let myNull = null;
+console.log(myNull);
+let userName = prompt("Введіть своє ім'я")
+alert("Привіт," + userName);
+let isConfirmed = confirm("Ви підтверджуєте дію?");
+ alert(isConfirmed? "Дякую за підтвердження!" : "Дію відмінено!");
+ 
+
+
